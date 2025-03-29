d9a1d8ab80223bb4e032b3f4e2c64737422d73f9
Herczeg Martinez <160034549+MartinezHrcz@users.noreply.github.com>
2025-03-29 20:33:26 +0100

    refact: created new class and split up the connection procedure into smaller pieces, thus making the code easier to read and to manage

---
+++ src/main/java/Database/Database.java
@@ -0,0 +1,120 @@
+package Database;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class Database {
+
+    private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USERNAME = "postgres";
+    private static String password = "";
+
+    private static Connection conn = null;
+    private static Driver driver = new org.postgresql.Driver();
+
+    public static void StartConnection() {
+        DriverConnection();
+
+        DriverRegister();
+
+        UserLogin();
+
+        DatabaseConnect();
+
+        ShowAllData();
+
+        Disconnect();
+    }
+
+    //Checks if the driver class exists
+    private static void DriverConnection() {
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        }
+        catch(ClassNotFoundException ex) {
+            System.out.println("Error: unable to load driver class!");
+            System.exit(1);
+        }
+
+    }
+
+    //Registers the driver
+    private static void DriverRegister() {
+
+        try{
+            DriverManager.registerDriver(driver);
+        } catch (SQLException e) {
+            System.out.println("Error: unable to register driver!");
+            System.exit(1);
+        }
+    }
+
+    //Asks for the user password
+    private static void UserLogin(){
+        System.out.println("Password for db: ");
+        Scanner sc = new Scanner(System.in);
+        password = sc.nextLine();
+    }
+
+    //Makes connection
+    private static void DatabaseConnect() {
+
+        try {
+            conn = DriverManager.getConnection(JDBC_URL,USERNAME,password);
+        } catch (SQLException e) {
+            System.out.println("Error: unable to connect to database!");
+            System.exit(1);
+        }
+
+    }
+
+    //Prints everything to the console
+    private static void ShowAllData(){
+        Statement stmt = null;
+
+        //Creates statement
+        try {
+            stmt = conn.createStatement();
+        } catch (SQLException e) {
+            System.out.println("Error: unable to execute statement!");
+            System.exit(1);
+        }
+
+        ResultSet rs = null;
+
+        //Exocutes the query
+        try {
+            rs = stmt.executeQuery("Select * from cars;");
+        } catch (SQLException e) {
+            System.out.println("Error: unable to execute statement!");
+            System.exit(1);
+        }
+
+        System.out.println("Database data:");
+
+        try {
+            while (rs.next()) {
+
+                String brand = rs.getString("brand");
+                String model = rs.getString("model");
+                int year = rs.getInt("year");
+                System.out.println(brand + " " + model + " " + year);
+
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error: unable to execute statement!");
+            System.exit(1);
+        }
+    }
+
+    //Disconnects from the database
+    private static void Disconnect() {
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,72 +1,12 @@
-import java.sql.*;
+import Database.Database;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String jdbcUrl = "jdbc:postgresql://localhost:5432/postgres";
-        String username = "postgres";
-        System.out.println("Password for db: ");
-        Scanner sc = new Scanner(System.in);
-        String password = sc.nextLine();
-
-        try {
-            Class.forName("org.postgresql.Driver");
-        }
-        catch(ClassNotFoundException ex) {
-            System.out.println("Error: unable to load driver class!");
-            System.exit(1);
-        }
-
-        Driver driver = new org.postgresql.Driver();
-
-        try{
-            DriverManager.registerDriver(driver);
-        } catch (SQLException e) {
-            System.out.println("Error: unable to register driver!");
-            System.exit(1);
-        }
-
-        Connection conn = null;
-
-        try {
-            conn = DriverManager.getConnection(jdbcUrl,username,password);
-        } catch (SQLException e) {
-            System.out.println("Error: unable to connect to database!");
-            System.exit(1);
-        }
-
-        Statement stmt = null;
-
-        try {
-            stmt = conn.createStatement();
-        } catch (SQLException e) {
-            System.out.println("Error: unable to execute statement!");
-            System.exit(1);
-        }
-
-        ResultSet rs = null;
-
-        try {
-            rs = stmt.executeQuery("Select * from cars;");
-        } catch (SQLException e) {
-            System.out.println("Error: unable to execute statement!");
-            System.exit(1);
-        }
-
-        System.out.println("Database Contents: ");
-
-        try {
-            while (rs.next()) {
-                String brand = rs.getString("brand");
-                String model = rs.getString("model");
-                int year = rs.getInt("year");
-                System.out.println(brand + " " + model + " " + year);
-            }
-
-        } catch (SQLException e) {
-            System.out.println("Error: unable to execute statement!");
-            System.exit(1);
-        }
-
+        Database.StartConnection();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Press any key to exit the program!");
+        scanner.nextLine();
     }
 }
